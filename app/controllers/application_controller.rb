b06b013ec8bdaed5ef40cb5963684c094a9efa9d
Alejandro <alejodiaz.123@gmail.com>
2020-04-16 22:05:02 -0400

    Validando si es administrador o no

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    def after_signed_path_for(resource)
+        if current_user.admin?
+            jams_path
+        else
+            home_index_path
+        end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_013020) do
+ActiveRecord::Schema.define(version: 2020_04_17_014511) do
 
   create_table "jams", force: :cascade do |t|
     t.string "name"
@@ -20,17 +20,7 @@ ActiveRecord::Schema.define(version: 2020_04_17_013020) do
     t.index ["user_id"], name: "index_jams_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "nickname"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'false' for column 'default'
 
 end
