8a42f9eddca9f9cf1b37b89e687fb13e2a407c2d
Alejandro <alejodiaz.123@gmail.com>
2020-04-16 21:37:22 -0400

    Agregando el nickname en el registro

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :jams
   get 'home/index'
-  devise_for :users
-  get 'players/index'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  #get 'players/index'
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
