4f7a773894545e42d3bf16d90e46e57e26d4cfc4
Alejandro <alejodiaz.123@gmail.com>
2020-04-16 21:15:27 -0400

    Agregando jam scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  resources :jams
+  get 'home/index'
   devise_for :users
   get 'players/index'
-  root 'players#index'
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
